0497ef222b948afad08d9027410000443d9319ed
Will Sanger <willsa@hey.com>
2021-02-03 16:24:38 -0600

    stepping back toward original to troubleshoot

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,8 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const localImages = require("./third_party/eleventy-plugin-local-images/.eleventy.js");
 const CleanCSS = require("clean-css");
+const GA_ID = require("./_data/metadata.json").googleAnalyticsId;
+
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
@@ -166,6 +168,9 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy(GA_ID ? "js" : "js/*[!cached].*");
+  // eleventyConfig.addPassthroughCopy("js");
+  // eleventyConfig.addPassthroughCopy("js/*[!cached].*");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("_headers");
 
